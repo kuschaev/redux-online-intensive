b7b8073807619f744058df59e9589b07d0ab04c2
Nikolai <nikolai.kuschaev@gmail.com>
2019-09-12 23:20:01 +0300

    Gallery nav actions implemented

---
+++ source/basic-redux/init/store.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { createLogger } from "redux-logger";
+
+import { rootReducer } from "./rootReducer";
+
+const logger = createLogger({
+    duration:  true,
+    collapsed: true,
+    colors:    {
+        title:     () => "#139BFE",
+        prevState: () => "#1C5FAF",
+        action:    () => "#149945",
+        nextState: () => "#A47104",
+        error:     () => "#FF0005",
+    },
+});
+
+const initialState = JSON.parse(localStorage.getItem('gallery'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devTools ? devTools : compose;
+const enhancedStore = composeEnhancers(applyMiddleware(logger));
+
+export const store = initialState ? createStore(rootReducer, initialState, enhancedStore) :
+    createStore(rootReducer, enhancedStore);
+
+store.subscribe(() => {
+    const state = store.getState();
+
+    localStorage.setItem('gallery', JSON.stringify(state));
+});
